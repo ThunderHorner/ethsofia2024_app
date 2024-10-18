9228f8e9f6dc001eac870062d2e42873447fb88c
ThunderHorner <slavov17@gmail.com>
2024-10-18 17:18:58 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ docker stop popit-wallet && docker rm popit-wallet
 docker build -t popit-wallet .
 
 # Run the container
-docker run -d -p 8080:8080 --name popit-wallet popit-wallet
+docker run -d -p 8089:8080 --name popit-wallet popit-wallet
