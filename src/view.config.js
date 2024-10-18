3d12bf9a3e53de4de2053f62a2822d55062b1c22
ThunderHorner <slavov17@gmail.com>
2024-10-18 17:26:09 +0300

    Create view.config.js

---
+++ src/view.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  devServer: {
+    disableHostCheck: true,  // To disable the host check
+    // OR use allowedHosts/public if you want to be more secure
+    // public: 'myapp.local',
+    // allowedHosts: ['myapp.local']
+  }
+}
